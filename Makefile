8c77bb567eeb1b2f78494b6673eb3cc2204ce7e0
Marco Aurélio Prado <marco.pdsv@gmail.com>
2024-02-07 17:14:38 -0300

    chore: add basic project structure

---
+++ Makefile
@@ -0,0 +1,5 @@
+start:
+	fvm flutter run -d chrome;
+	
+build-web:
+	fvm flutter build web;
---
+++ android/app/src/main/kotlin/tech/binarydream/portfolio/portfolio/MainActivity.kt
@@ -0,0 +1,6 @@
+package tech.binarydream.portfolio.portfolio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
